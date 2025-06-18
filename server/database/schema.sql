b7d50b250b036076d6ac99c5cb5b7073ff28d1b3
G-Ludovic <l.galicher@free.fr>
2025-06-18 11:28:16 +0200

    work in progress database

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,21 +1,65 @@
-create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+CREATE TABLE user (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  identifiant VARCHAR(150),
+  password VARCHAR(255) NOT NULL
 );
 
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
+CREATE TABLE parent (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  firstname VARCHAR(50),
+  lastname VARCHAR(50),
+  address VARCHAR(250),
+  phone_number VARCHAR(15),
+  user_id INT,
+  FOREIGN KEY(user_id),
+  REFERENCES user(id)
 );
 
-insert into user(id, email, password)
-values
-  (1, "jdoe@mail.com", "123456");
+CREATE TABLE kid (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  firstname VARCHAR(50),
+  lastname VARCHAR(50),
+  age INT NOT NULL,
+  walker BOOLEAN,
+  allergy VARCHAR(250),
+  handicap BOOLEAN,
+  parent_id INT,
+  FOREIGN KEY(parent_id),
+  REFERENCES parent(id)
+);
 
-insert into item(id, title, user_id)
-values
-  (1, "Stuff", 1),
-  (2, "Doodads", 1);
+CREATE TABLE reservation (
+  PRIMARY KEY (kid_id, nursery_id),
+  date DATE
+  is_validated BOOLEAN,
+  kid_id INT,
+  nursery_id INT,
+  FOREIGN KEY (nursery_id),
+  REFERENCES nursery(id),
+  FOREIGN KEY (kid_id),
+  REFERENCES kid(id)
+);
+
+CREATE TABLE nursery (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name VARCHAR(150),
+  address VARCHAR(250),
+  phone_number VARCHAR(15),
+  descripton VARCHAR(300),
+  capacity INT,
+  user_id INT,
+  FOREIGN KEY(user_id),
+  REFERENCES user(id)
+);
+
+INSERT INTO user (id, identifiant, password)
+VALUES
+  (1, "john@gmail.com", "1234"),
+  (2, "nina@gmail.com", "5678"),
+  (3, "axel@gmail.com", "9123");
+
+INSERT INTO parent (id, firstname, lastname, adress, phone_number)
+VALUES
+  (1, "john", "rambo", "2 rue des favelas, 98500 Juven", "06 55 99 99 99"),
+  (2, "nina", "williams", "98 rue du fightclub, 53999 Aren", "06 54 99 99 99"),
+  
