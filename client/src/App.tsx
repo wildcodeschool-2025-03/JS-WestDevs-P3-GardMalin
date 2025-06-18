02be9e959d2d48983d3db531291e02af062d17cc
corentinthebaud <corentin.thebaud44@gmail.com>
2025-06-18 11:32:36 +0200

    Footer done miss typography

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <h1>Bienvenue dans l'application</h1>
+      <Footer />
     </>
   );
 }
