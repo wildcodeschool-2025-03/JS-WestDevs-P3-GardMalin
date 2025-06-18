43e6c0ab517192c7c243096ea1bc6141f069b975
Romain-dev85 <r.auvinet@gmail.com>
2025-06-18 10:20:55 +0200

    creation header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <h1>Bienvenue dans l'application</h1>
     </>
   );
---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header-container">
+      <img src="./images/gardmalin-logo.png" alt="logo" />
+    </header>
+  );
+};
+
+export default Header;
