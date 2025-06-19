80e2cd2daa97f722946fd004b082450fe27886ee
G-Ludovic <l.galicher@free.fr>
2025-06-19 15:18:19 +0200

    create sql table

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,8 +10,8 @@ CREATE TABLE parent (
   lastname VARCHAR(50),
   address VARCHAR(250),
   phone_number VARCHAR(15),
-  user_id INT,
-  FOREIGN KEY(user_id),
+  user_id INT UNSIGNED,
+  FOREIGN KEY(user_id)
   REFERENCES user(id)
 );
 
@@ -23,21 +23,9 @@ CREATE TABLE kid (
   walker BOOLEAN,
   allergy VARCHAR(250),
   handicap BOOLEAN,
-  parent_id INT,
-  FOREIGN KEY(parent_id),
+  parent_id INT UNSIGNED,
+  FOREIGN KEY(parent_id)
   REFERENCES parent(id)
-);
-
-CREATE TABLE reservation (
-  PRIMARY KEY (kid_id, nursery_id),
-  date DATE
-  is_validated BOOLEAN,
-  kid_id INT,
-  nursery_id INT,
-  FOREIGN KEY (nursery_id),
-  REFERENCES nursery(id),
-  FOREIGN KEY (kid_id),
-  REFERENCES kid(id)
 );
 
 CREATE TABLE nursery (
@@ -47,31 +35,43 @@ CREATE TABLE nursery (
   phone_number VARCHAR(15),
   descripton VARCHAR(300),
   capacity INT,
-  user_id INT,
-  FOREIGN KEY(user_id),
+  user_id INT UNSIGNED,
+  FOREIGN KEY(user_id)
   REFERENCES user(id)
 );
 
-INSERT INTO user (id, identifiant, password)
-VALUES
-  (1, "john@gmail.com", "1234"),
-  (2, "nina@gmail.com", "5678"),
-  (3, "axel@gmail.com", "9123");
+CREATE TABLE reservation (
+  PRIMARY KEY (kid_id, nursery_id),
+  date DATE,
+  is_validated BOOLEAN,
+  kid_id INT UNSIGNED,
+  nursery_id INT UNSIGNED,
+  FOREIGN KEY (nursery_id)
+  REFERENCES nursery(id),
+  FOREIGN KEY (kid_id)
+  REFERENCES kid(id)
+);
 
-INSERT INTO parent (id, firstname, lastname, adress, phone_number)
+INSERT INTO user (identifiant, password)
 VALUES
-  (1, "John", "Wick", "2 rue des favelas, 98500 Juven", "06 55 99 99 99"),
-  (2, "Nina", "Williams", "98 rue du fightclub, 53999 Aren", "06 54 99 99 99"),
-  (3, "Axel", "Okalm", "55 rue de la zenitude, 25599", "06 53 99 99 99" );
+  ("john@gmail.com", "$argon2id$v=19$m=16,t=2,p=1$NkVScVltZmFJaXBFTlhaYQ$XvoBBLr+0DEPuB7W/4BfWQ"),
+  ("nina@gmail.com", "$argon2id$v=19$m=16,t=2,p=1$V0pPYXdXeFFzWGdUbjZ1SA$SuoHm8nIke7Q3Adt4qfefA"),
+  ("axel@gmail.com", "$argon2id$v=19$m=16,t=2,p=1$a3NyTTVCMlFkR2Z3OXZWdQ$QXb8VjSH8aXAcBBmODOv5Q");
 
-INSERT INTO kid (id, firstname, lastname, age, walker, allergy, handicap)
+INSERT INTO parent (firstname, lastname, address, phone_number)
 VALUES
-  (1, "Flora", "Wick", "8", true, "aucune", false),
-  (2, "Shana", "Williams", "1" false, "asthme", false),
-  (3, "Louis", "Okalm", "5", true, "aucune", true);
+  ("John", "Wick", "2 rue des favelas, 98500 Juven", "06 55 99 99 99"),
+  ("Nina", "Williams", "98 rue du fightclub, 53999 Aren", "06 54 99 99 99"),
+  ("Axel", "Okalm", "55 rue de la zenitude, 25599", "06 53 99 99 99" );
 
-INSERT INTO nursery (id, name, adress, phone_number, capacity)
+INSERT INTO kid (firstname, lastname, age, walker, allergy, handicap)
 VALUES
-  (1, "Les Petits Explorateurs", "12 rue des érables, 75000 Paris" , "01 23 45 67 89", "50 places"),
-  (2, "Les Coccinelles Joyeuses", "5 allée des Acacias, 69000 Lyon", "04 78 12 34 56", "25 places"),
-  (3, "Puzzle & Petits Pas", "22 rue des Tisserands 31000 Toulouse", "05 61 23 45 67", "128 places");
+  ("Flora", "Wick", "8", 1, "aucune", 0),
+  ("Shana", "Williams", "1", 0, "asthme", 0),
+  ("Louis", "Okalm", "5", 1, "aucune", 1);
+
+INSERT INTO nursery (name, address, phone_number, capacity)
+VALUES
+  ("Les Petits Explorateurs", "12 rue des érables, 75000 Paris" , "01 23 45 67 89", 50),
+  ("Les Coccinelles Joyeuses", "5 allée des Acacias, 69000 Lyon", "04 78 12 34 56", 25),
+  ("Puzzle & Petits Pas", "22 rue des Tisserands 31000 Toulouse", "05 61 23 45 67", 128);
