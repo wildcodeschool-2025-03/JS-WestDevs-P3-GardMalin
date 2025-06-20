5330c29bcee8aceca3e8c935d7fe97f1217d9f0a
Estelle Michard <michardestelleem@gmail.com>
2025-06-20 16:04:28 +0200

    Maintenance page is done

--- client/src/pages/PageEnConstruction/PageEnConstruction.tsx
+++ client/src/pages/PageEnConstruction/PageEnConstruction.tsx
@@ -1,26 +1,11 @@
 import "./PageEnConstruction.css";
-import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 
 function PageEnConstruction() {
-  const [seconds, setSeconds] = useState(300);
   const navigate = useNavigate();
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const newSeconds = seconds - 1;
-      setSeconds(newSeconds);
-
-      if (newSeconds <= 0) {
-        clearInterval(interval);
-        navigate(-1);
-      }
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, [seconds, navigate]);
-
-  const displaySeconds = seconds < 10 ? `0${seconds}` : seconds;
+  const pagePrecedente = () => {
+    navigate(-1);
+  };
 
   return (
     <section className="page-en-construction">
@@ -39,10 +24,9 @@ function PageEnConstruction() {
         <p>
           Cette page est en cours de maintenance, merci de votre compréhension !
         </p>
-        <p id="chrono-oups">
-          Vous allez être rediriger dans {displaySeconds} secondes <br />
-          vers la page précédente.
-        </p>
+        <button type="button" onClick={pagePrecedente}>
+          Page précédente
+        </button>
       </article>
     </section>
   );
