40080f57dad0434cd3e4290f73c707d47b3a958b
Romain-dev85 <r.auvinet@gmail.com>
2025-06-23 15:22:56 +0200

    html modifications made

--- client/src/pages/LoginPro/LoginPro.tsx
+++ client/src/pages/LoginPro/LoginPro.tsx
@@ -3,47 +3,40 @@ import "./LoginPro.css";
 
 const LoginPro = () => {
   return (
-    <>
-      <main className="login-pro-container">
-        <section className="picture-pro-container">
-          <h1>Gérez ici votre espace Pro comme un pro</h1>
-          <img src="./images/login_pro.png" alt="parents avec des enfants" />
-        </section>
-        <section>
-          <img src="./images/gardmalin-logo.png" alt="logo gardmalin" />
-          <h1>Gérez ici votre espace Pro comme un pro</h1>
-          <h2>Connexion</h2>
-          <form className="form-pro-container">
-            <label htmlFor="email" hidden>
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              placeholder="Adresse mail"
-              required
-            />
-            <label htmlFor="password" hidden>
-              {" "}
-              Votre mot de passe
-            </label>
-            <input
-              type="password"
-              id="password"
-              placeholder="Mot de passe"
-              required
-            />
-            <label htmlFor="checkbox">
-              <input type="checkbox" id="checkbox" />
-              <Link to="/">Conditions d'utilisation</Link>
-            </label>
-            <button type="submit">Se connecter</button>
-            <p>Vous n'avez pas de compte ?</p>
-            <Link to="/">Cliquez ici !</Link>
-          </form>
-        </section>
-      </main>
-    </>
+    <main className="login-pro-container">
+      <section className="picture-pro-container">
+        <h1>Gérez ici votre espace Pro comme un pro</h1>
+        <img src="/images/login_pro.png" alt="nanny with children" />
+      </section>
+      <section>
+        <img src="/images/gardmalin-logo.png" alt="logo gardmalin" />
+        <h1>Gérez ici votre espace Pro comme un pro</h1>
+        <h2>Connexion</h2>
+        <form className="form-pro-container">
+          <label htmlFor="email" hidden>
+            Email
+          </label>
+          <input type="email" id="email" placeholder="Adresse mail" required />
+          <label htmlFor="password" hidden>
+            {" "}
+            Votre mot de passe
+          </label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Mot de passe"
+            required
+          />
+          <label htmlFor="checkbox">
+            <input type="checkbox" id="checkbox" />
+            <Link to="/">Conditions d'utilisation</Link>
+          </label>
+          <button type="submit">Se connecter</button>
+          <p>Vous n'avez pas de compte ?</p>
+          <Link to="/">Cliquez ici !</Link>
+        </form>
+      </section>
+    </main>
   );
 };
 
