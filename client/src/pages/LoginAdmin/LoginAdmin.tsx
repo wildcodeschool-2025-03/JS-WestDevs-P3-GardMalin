113cd4440afa81699c84e5c1fa09a20300d08ff4
Romain-dev85 <r.auvinet@gmail.com>
2025-06-23 15:03:32 +0200

    CSS/HTML corrected

--- client/src/pages/LoginAdmin/LoginAdmin.tsx
+++ client/src/pages/LoginAdmin/LoginAdmin.tsx
@@ -3,50 +3,40 @@ import "./LoginAdmin.css";
 
 const LoginAdmin = () => {
   return (
-    <>
-      <main className="login-admin-container">
-        <section className="picture-admin-container">
-          <h1>Espace administrateur</h1>
-          <img
-            src="./images/login_admin.png"
-            alt="admloginadmins avec un enfant"
+    <main className="login-admin-container">
+      <section className="picture-admin-container">
+        <h1>Espace administrateur</h1>
+        <img src="/images/login_admin.png" alt="avatar of a man" />
+      </section>
+      <section>
+        <img src="/images/gardmalin-logo.png" alt="logo gardmalin" />
+        <h1>Espace administrateur</h1>
+        <h2>Connexion</h2>
+        <form className="form-admin-container">
+          <label htmlFor="email" hidden>
+            Email
+          </label>
+          <input type="email" id="email" placeholder="Adresse mail" required />
+          <label htmlFor="password" hidden>
+            {" "}
+            Votre mot de passe
+          </label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Mot de passe"
+            required
           />
-        </section>
-        <section>
-          <img src="./images/gardmalin-logo.png" alt="logo gardmalin" />
-          <h1>Espace administrateur</h1>
-          <h2>Connexion</h2>
-          <form className="form-admin-container">
-            <label htmlFor="email" hidden>
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              placeholder="Adresse mail"
-              required
-            />
-            <label htmlFor="password" hidden>
-              {" "}
-              Votre mot de passe
-            </label>
-            <input
-              type="password"
-              id="password"
-              placeholder="Mot de passe"
-              required
-            />
-            <label htmlFor="checkbox">
-              <input type="checkbox" id="checkbox" />
-              <Link to="/">Conditions d'utilisation</Link>
-            </label>
-            <button type="submit">Se connecter</button>
-            <p>Mot de passe oublié ?</p>
-            <Link to="/">Contactez-nous ici</Link>
-          </form>
-        </section>
-      </main>
-    </>
+          <label htmlFor="checkbox">
+            <input type="checkbox" id="checkbox" />
+            <Link to="/">Conditions d'utilisation</Link>
+          </label>
+          <button type="submit">Se connecter</button>
+          <p>Mot de passe oublié ?</p>
+          <Link to="/">Contactez-nous ici</Link>
+        </form>
+      </section>
+    </main>
   );
 };
 
