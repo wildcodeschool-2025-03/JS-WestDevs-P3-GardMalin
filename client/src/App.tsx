1a9a20ea9cacf490135ee163f2489578bd3c956a
corentinthebaud <corentin.thebaud44@gmail.com>
2025-06-23 11:28:48 +0200

    fixbug done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router";
 import "./App.css";
 
-import Footer from "./components/Footer";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
 function App() {
--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router";
-import "./footer.css";
+import "./Footer.css";
 
 function Footer() {
   return (
