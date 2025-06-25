1c3961a929bdd3e2245d9bcbe34988b7cc592ccc
Romain-dev85 <r.auvinet@gmail.com>
2025-06-25 15:54:56 +0200

    creation page parent information

--- client/src/pages/ParentInformation/ParentInformation.tsx
+++ client/src/pages/ParentInformation/ParentInformation.tsx
@@ -20,7 +20,6 @@ const ParentInformation = () => {
     fetch("http://localhost:3310/api/parents")
       .then((res) => res.json())
       .then((data) => setParents(data));
-    console.warn(parents);
 
     fetch("http://localhost:3310/api/kids")
       .then((res) => res.json())
@@ -30,7 +29,7 @@ const ParentInformation = () => {
   return (
     <main className="parent-info-container">
       <h1>Espace parents informations</h1>
-      <section className="parent-info-section-1">
+      <section className="parent-info-section">
         <img src="/images/family.png" alt="family avatar" />
         <h2>Profil parent</h2>
         <ul>
@@ -39,7 +38,7 @@ const ParentInformation = () => {
           ))}
         </ul>
       </section>
-      <section className="parent-info-section-2">
+      <section className="enfant-info-section">
         <h2>Profil(s) enfant(s)</h2>
         <article>
           <img src="/images/little_girl.png" alt="avatar of a girl" />
@@ -51,10 +50,15 @@ const ParentInformation = () => {
         </article>
         <article>
           <img src="/images/little_boy.png" alt="avatar of a boy" />
+          <ul>
+            {kids.map((kid) => (
+              <li key={kid.id}>{kid.firstname}</li>
+            ))}
+          </ul>
         </article>
+        <h2>Vous voulez inscrire un nouvel enfant ?</h2>
+        <Link to="/">Fichier inscription</Link>
       </section>
-      <h2>Vous voulez inscrire un nouvel enfant ?</h2>
-      <Link to="/">Fichier inscription</Link>
     </main>
   );
 };
