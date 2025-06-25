ee3163483cc8069f641d359bf9542851f667d779
corentinthebaud <corentin.thebaud44@gmail.com>
2025-06-25 09:59:06 +0200

    work in progress

--- client/src/pages/RegistrationParentsPage/RegistrationParentsPage.tsx
+++ client/src/pages/RegistrationParentsPage/RegistrationParentsPage.tsx
@@ -3,7 +3,7 @@ import "./RegistrationParentsPage.css";
 function RegistrationParentsPage() {
   return (
     <main className="registration-parents-page">
-      <section className="section-page">
+      <section className="section-parents-page">
         <div>
           <img src="/images/gardmalin-logo.png" alt="logo Gardmalin" />
         </div>
@@ -14,31 +14,74 @@ function RegistrationParentsPage() {
 
         <section className="form-parents">
           <form className="registration-parents">
-            <input type="text" placeholder=" Nom" />
-
-            <input type="text" placeholder=" Prénom" />
-
-            <input type="email" placeholder=" Adresse email" />
-
-            <input type="date" placeholder="Date de naissance" />
-
-            <input type="address" placeholder="Adresse" />
-
-            <input type="tel" placeholder=" Numéro de téléphone" />
-
-            <input type="password" placeholder=" Mot de passe" />
-
-            <input type="text" placeholder=" Confirmer cotre mot de passe" />
-
+            <article className="grid-box-parents1">
+              <label htmlFor="name" hidden>
+                Nom
+              </label>
+              <input id="name" type="text" placeholder="Nom" />
+              <label htmlFor="firstname" hidden>
+                Prénom
+              </label>
+              <input id="firstname" type="text" placeholder="Prénom" />
+              <label htmlFor="email" hidden>
+                Adresse email
+              </label>
+              <input id="email" type="email" placeholder="Adresse email" />
+              <label htmlFor="birthday">Date de naissance</label>
+              <input
+                id="birthday"
+                type="date"
+                placeholder="Date de naissance"
+              />
+              <label htmlFor="address" hidden>
+                Adresse
+              </label>
+              <input id="address" type="text" placeholder="Adresse" />
+            </article>
+            <article className="grid-box-parents2">
+              <label htmlFor="city" hidden>
+                Ville
+              </label>
+              <input id="city" type="text" placeholder="Ville" />
+              <label htmlFor="postal" hidden>
+                Code postale
+              </label>
+              <input id="postal" type="number" placeholder="Code postale" />
+              <label htmlFor="phone" hidden>
+                Numéro de téléphone
+              </label>
+              <input id="phone" type="tel" placeholder="Numéro de téléphone" />
+              <label htmlFor="password" hidden>
+                Mot de passe
+              </label>
+              <input id="password" type="password" placeholder="Mot de passe" />
+              <label htmlFor="confirm-password" hidden>
+                Confirmer votre mot de passe
+              </label>
+              <input
+                id="confirm-password"
+                type="text"
+                placeholder="Confirmer cotre mot de passe"
+              />
+            </article>
             <label htmlFor="file">
               Mettez en format PDF une copie de votre livret de famille
             </label>
 
-            <input type="file" name="livretPdf" accept="application/pdf" />
+            <input
+              id="file"
+              type="file"
+              name="livretPdf"
+              accept="application/pdf"
+            />
 
-            <button type="submit">Confirmer votre incription</button>
+            <button className="submit-parents" type="submit">
+              Confirmer votre incription
+            </button>
           </form>
-          <button type="button">Espace parents</button>
+          <button className="parents-button" type="button">
+            Espace parents
+          </button>
         </section>
       </section>
     </main>
