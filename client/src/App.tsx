047d4daaab4f72cd009b269ec990a756dffde97e
corentinthebaud <corentin.thebaud44@gmail.com>
2025-06-25 14:41:51 +0200

    registration parents done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,12 @@ import "./App.css";
 
 import Footer from "./components/Footer";
 import Header from "./components/Header/Header";
-import SubscribeParentsPage from "./pages/RegistrationParentsPage/RegistrationParentsPage";
 
 function App() {
   return (
     <>
       <Header />
       <h1>Bienvenue dans l'application</h1>
-      <SubscribeParentsPage />
       <Footer />
     </>
   );
--- client/src/pages/RegistrationParentsPage/RegistrationParentsPage.tsx
+++ client/src/pages/RegistrationParentsPage/RegistrationParentsPage.tsx
@@ -1,6 +1,18 @@
+import { type ChangeEvent, useState } from "react";
+import { Link } from "react-router";
 import "./RegistrationParentsPage.css";
 
 function RegistrationParentsPage() {
+  const [fileName, setFileName] = useState("Aucun fichier sélectionné");
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setFileName(e.target.files[0].name);
+    } else {
+      setFileName("Aucun fichier sélectionné");
+    }
+  };
+
   return (
     <main className="registration-parents-page">
       <section className="section-parents-page">
@@ -8,7 +20,7 @@ function RegistrationParentsPage() {
           <img src="/images/gardmalin-logo.png" alt="logo Gardmalin" />
         </div>
 
-        <div className="title-registration">
+        <div className="title-registration-parents">
           <h1>Création de votre espace parents</h1>
         </div>
 
@@ -27,11 +39,15 @@ function RegistrationParentsPage() {
                 Adresse email
               </label>
               <input id="email" type="email" placeholder="Adresse email" />
-              <label htmlFor="birthday">Date de naissance</label>
+              <label htmlFor="birthday" hidden>
+                Date de naissance
+              </label>
               <input
                 id="birthday"
-                type="date"
-                placeholder="Date de naissance"
+                type="text"
+                placeholder="Date de naissance ex : 01/01/1901"
+                pattern="\d{2}/\d{2}/\d{4}"
+                title="Format attendu : jj/mm/aaaa"
               />
               <label htmlFor="address" hidden>
                 Adresse
@@ -60,28 +76,32 @@ function RegistrationParentsPage() {
               </label>
               <input
                 id="confirm-password"
-                type="text"
+                type="password"
                 placeholder="Confirmer cotre mot de passe"
               />
             </article>
-            <label htmlFor="file">
+            <label htmlFor="select-file" className="custom-file-label">
               Mettez en format PDF une copie de votre livret de famille
             </label>
-
+            <span className="file-name">{fileName}</span>
             <input
-              id="file"
+              id="select-file"
               type="file"
               name="livretPdf"
               accept="application/pdf"
+              onChange={handleFileChange}
+              hidden
             />
 
             <button className="submit-parents" type="submit">
               Confirmer votre incription
             </button>
           </form>
-          <button className="parents-button" type="button">
-            Espace parents
-          </button>
+          <Link to="">
+            <button className="parents-button" type="button">
+              Espace parents
+            </button>
+          </Link>
         </section>
       </section>
     </main>
