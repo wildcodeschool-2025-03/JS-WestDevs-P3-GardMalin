fb9910b9bf5e49500b65f7a7cebfab3e1c728640
G-Ludovic <l.galicher@free.fr>
2025-06-26 16:31:48 +0200

    work in progress

---
+++ client/src/pages/RegistrationChildrenPage/RegistrationChildrenPage.tsx
@@ -0,0 +1,11 @@
+import "./RegistrationChildrenPage.css";
+
+function RegistrationChildrenPage() {
+  return (
+    <main className="page-registration-children">
+      <h1>Inscription de l'enfant</h1>
+    </main>
+  );
+}
+
+export default RegistrationChildrenPage;
