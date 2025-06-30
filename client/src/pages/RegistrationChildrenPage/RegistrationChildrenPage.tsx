b1594bedd7fdb145e863244265aaf82107eb021a
G-Ludovic <l.galicher@free.fr>
2025-06-30 16:11:08 +0200

    work in progress

--- client/src/pages/RegistrationChildrenPage/RegistrationChildrenPage.tsx
+++ client/src/pages/RegistrationChildrenPage/RegistrationChildrenPage.tsx
@@ -9,14 +9,16 @@ function RegistrationChildrenPage() {
       </section>
 
       <section className="form-section">
-        <form>
-          <label htmlFor="text">Sexe de l'enfant</label>
-          <br />
-          <label htmlFor="checkbox">Féminin</label>
-          <input type="checkbox" id="féminin" value="féminin" />
-          <label htmlFor="checkbox">Masculin</label>
-          <input type="checkbox" id="masculin" value="masculin" />
-          <br />
+        <form className="fields">
+          <article className="gender">
+            <label htmlFor="text">Sexe de l'enfant</label>
+            <br />
+            <label htmlFor="checkbox">Féminin</label>
+            <input type="checkbox" id="féminin" value="féminin" />
+            <label htmlFor="checkbox">Masculin</label>
+            <input type="checkbox" id="masculin" value="masculin" />
+          </article>
+
           <label htmlFor="text" hidden>
             Nom
           </label>
@@ -32,32 +34,37 @@ function RegistrationChildrenPage() {
           </label>
           <input type="text" id="firstname" placeholder="Age" required />
           <br />
-          <label htmlFor="text">Handicap ou spécificités</label>
-          <br />
-          <input type="checkbox" id="yes" value="yes" />
-          <label htmlFor="">Oui</label>
-          <label htmlFor="checkbox">
-            <input type="checkbox" id="checkbox" />
-            Non
-          </label>
-          <br />
+
+          <article className="handicap">
+            <label htmlFor="text">Handicap ou spécificités</label>
+            <br />
+            <input type="checkbox" id="yes" value="yes" />
+            <label htmlFor="">Oui</label>
+            <label htmlFor="checkbox">
+              <input type="checkbox" id="checkbox" />
+              Non
+            </label>
+          </article>
+
           <label htmlFor="text" hidden>
             Allergies
           </label>
           <input type="text" id="firstname" placeholder="Allergies" />
           <br />
-          <label htmlFor="text">Est-ce que votre enfant marche ?</label>
-          <br />
-          <input type="checkbox" id="yes" value="yes" />
-          <label htmlFor="">Oui</label>
-          <input type="checkbox" id="no" value="no" />
-          <label htmlFor="">Non</label>
+          <article className="walk">
+            <label htmlFor="text">Est-ce que votre enfant marche ?</label>
+            <br />
+            <input type="checkbox" id="yes" value="yes" />
+            <label htmlFor="">Oui</label>
+            <input type="checkbox" id="no" value="no" />
+            <label htmlFor="">Non</label>
+          </article>
 
           <button className="validated" type="submit">
             Valider
           </button>
-          <p>Vous n'avez pas de compte ?</p>
         </form>
+
         <section className="footer-registration-children">
           <button className="return-button" type="button">
             Retour
