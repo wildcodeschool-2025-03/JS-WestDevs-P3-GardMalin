578c829a17c5e34d58c5fa4f199b57669bd78621
Romain-dev85 <r.auvinet@gmail.com>
2025-07-04 09:50:52 +0200

    work done

--- server/src/utils/auth.ts
+++ server/src/utils/auth.ts
@@ -1,6 +1,7 @@
 import argon2 from "argon2";
 import type { RequestHandler } from "express";
 import usersRepository from "../modules/users/usersRepository";
+import jwt from "jsonwebtoken";
 
 const hashPassword: RequestHandler = async (req, res, next) => {
   try {
@@ -35,6 +36,24 @@ const login: RequestHandler = async (req, res) => {
     if (!isPasswordValid) {
       throw new Error("Invalid password");
     }
+
+    const payload = {
+      id: user.id,
+      email: user.email,
+    };
+
+    const secretKey = process.env.APP_SECRET;
+
+    if (!secretKey) {
+      throw new Error("A secret must be provided");
+    }
+
+    const token = jwt.sign(payload, secretKey, { expiresIn: "1d" });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+    });
 
     res.status(200).json("Contragulations, you're logged in !");
   } catch (err) {
