3c02995effd6a80a8f9be38e0f9647aa19757ab6
corentinthebaud <corentin.thebaud44@gmail.com>
2025-07-09 15:56:42 +0200

    request add and request readById for table kid is done

--- server/src/modules/kids/kidsActions.ts
+++ server/src/modules/kids/kidsActions.ts
@@ -6,4 +6,36 @@ const browse: RequestHandler = async (req, res) => {
   res.json(result);
 };
 
-export default { browse };
+const add: RequestHandler = async (req, res) => {
+  try {
+    const kid = await kidsRepository.create(req.body);
+
+    if (kid) {
+      res
+        .status(201)
+        .json("Congratulation your kid has been created in your profile !");
+    } else {
+      res
+        .status(404)
+        .json("You are doing a mistake during your registration kid ");
+    }
+  } catch (err) {
+    res.sendStatus(500);
+  }
+};
+
+const read: RequestHandler = async (req, res) => {
+  try {
+    const result = await kidsRepository.readById(req.params.id);
+
+    if (result) {
+      res.json(result);
+    } else {
+      res.status(404).send("This ID doesn't exist !");
+    }
+  } catch {
+    res.sendStatus(500);
+  }
+};
+
+export default { browse, add, read };
