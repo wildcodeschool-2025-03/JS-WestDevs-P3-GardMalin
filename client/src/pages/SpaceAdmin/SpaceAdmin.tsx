26c4aaad7fbe8328d361e29d32db6d8dbc2fe9ca
G-Ludovic <l.galicher@free.fr>
2025-07-17 14:15:31 +0200

    work in progress

--- client/src/pages/SpaceAdmin/SpaceAdmin.tsx
+++ client/src/pages/SpaceAdmin/SpaceAdmin.tsx
@@ -11,7 +11,7 @@ function SpaceAdmin() {
           <h1>Espace Administrateur</h1>
         </header>
 
-        <section className="input-search">
+        <article className="input-search">
           <input
             type="text"
             value=""
@@ -19,40 +19,23 @@ function SpaceAdmin() {
             aria-label="Parents ou professionnels"
             placeholder="Parents ou professionnels"
           />
-        </section>
+        </article>
 
-        <section className="display-results">
-          <article className="article-parents">
-            <h3>Parents</h3>
-            {/* <table>
-              <tr>
-                <td>Prénoms</td>
-                <td>Noms</td>
-                <td>ville</td>
-                <td>10</td>
-                <td>5</td>
-              </tr>
-              <tr>
-                <td>Propriétaire</td>
-                <td>Belle-mère</td>
-                <td>Moi</td>
-                <td>Moi</td>
-                <td>Belle-sœur</td>
-              </tr>
-              <tr>
-                <td>Habitudes alimentaires</td>
-                <td>Mange tous les restes</td>
-                <td>Grignote la nourriture</td>
-                <td>Mange copieusement</td>
-                <td>Mange jusqu'à ce qu'il éclate</td>
-              </tr>
-            </table> */}
-          </article>
+        <article className="article-parents">
+          <h3>Parents</h3>
+          <table>
+            <tr>Parent 1</tr>
+            <tr>Parent 2</tr>
+          </table>
+        </article>
 
-          <article className="article-pro">
-            <h3>Professionnels</h3>
-          </article>
-        </section>
+        <article className="article-pro">
+          <h3>Professionnels</h3>
+          <table>
+            <tr>Crèche 1</tr>
+            <tr>Crèche 2</tr>
+          </table>
+        </article>
       </div>
     </>
   );
