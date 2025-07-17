821378ec147c7b3c97f36990fa57e2798eb50e08
Romain-dev85 <r.auvinet@gmail.com>
2025-07-17 12:02:43 +0200

    work done

--- client/src/pages/LoginAdmin/LoginAdmin.tsx
+++ client/src/pages/LoginAdmin/LoginAdmin.tsx
@@ -1,8 +1,44 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import "./LoginAdmin.css";
+import { toast } from "react-toastify";
 import BackButton from "../../components/BackButton/BackButton";
+import { useAuth } from "../../services/AuthContext";
 
 const LoginAdmin = () => {
+  const { setIsLogged, setUser } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = (formData: FormData) => {
+    const data = Object.fromEntries(formData);
+
+    fetch("http://localhost:3310/api/login", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          toast.error("Echec de connexion");
+          throw new Error("Connexion failed");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.role !== "admin") {
+          toast.error("Accès refusé : rôle non autorisé");
+          return;
+        }
+
+        toast.success("Vous êtes connecté");
+        setIsLogged(true);
+        setUser(data);
+        navigate("/space-admin");
+      });
+  };
+
   return (
     <div className="login-admin-container">
       <section>
@@ -13,23 +49,29 @@ const LoginAdmin = () => {
         <img src="/images/gardmalin-logo.png" alt="logo gardmalin" />
         <h1>Espace administrateur</h1>
         <h2>Connexion</h2>
-        <form className="form-admin-container">
+        <form action={handleSubmit} className="form-admin-container">
           <label htmlFor="email" hidden>
             Email
           </label>
-          <input type="email" id="email" placeholder="Adresse mail" required />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Adresse mail"
+            required
+          />
           <label htmlFor="password" hidden>
-            {" "}
             Votre mot de passe
           </label>
           <input
             type="password"
             id="password"
+            name="password"
             placeholder="Mot de passe"
             required
           />
           <label htmlFor="checkbox">
-            <input type="checkbox" id="checkbox" />
+            <input type="checkbox" id="checkbox" required />
             <Link to="/">Conditions d'utilisation</Link>
           </label>
           <button type="submit">Se connecter</button>
