af0889fba8edfc3c0d1f6d5dd2aaede5f0fecf68
G-Ludovic <l.galicher@free.fr>
2025-07-17 11:27:41 +0200

    work in progress

---
+++ client/src/pages/SpaceAdmin/SpaceAdmin.tsx
@@ -0,0 +1,15 @@
+import "./SpaceAdmin.css";
+
+function SpaceAdmin() {
+  return (
+    <>
+      <div className="body-admin">
+        <header className="header-space-admin">
+          <h1>Espace Administrateur</h1>
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default SpaceAdmin;
