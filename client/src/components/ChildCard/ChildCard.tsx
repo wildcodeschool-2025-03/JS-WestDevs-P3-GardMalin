7cee8172a0db8736aa166c6fbc07e57b82674fa8
Estelle Michard <michardestelleem@gmail.com>
2025-07-29 17:14:02 +0200

    Fixbug-CSS is done

--- client/src/components/ChildCard/ChildCard.tsx
+++ client/src/components/ChildCard/ChildCard.tsx
@@ -5,7 +5,6 @@ import { toast } from "react-toastify";
 
 const ChildCard = ({
   id,
-  imgSrc,
   firstname,
   lastname,
   age,
@@ -25,6 +24,46 @@ const ChildCard = ({
     allergy,
     walker,
   });
+
+  const imgGirl = [
+    "/images/little-girl/little-girl2.png",
+    "/images/little-girl/little-girl3.png",
+    "/images/little-girl/little-girl4.png",
+    "/images/little-girl/little-girl5.png",
+    "/images/little-girl/little-girl6.png",
+    "/images/little-girl/little-girl7.png",
+    "/images/little-girl/little-girl8.png",
+    "/images/little-girl/little-girl-9.png",
+    "/images/little-girl/little-girl10.png",
+    "/images/little-girl/little-girl11.png",
+    "/images/little-girl/little-girl12.png",
+    "/images/little-girl/little-girl13.png",
+  ];
+
+  const imgBoy = [
+    "/images/little-boy/little-boy2.png",
+    "/images/little-boy/little-boy3.png",
+    "/images/little-boy/little-boy4.png",
+    "/images/little-boy/little-boy5.png",
+    "/images/little-boy/little-boy6.png",
+    "/images/little-boy/little-boy6.png",
+    "/images/little-boy/little-boy7.png",
+    "/images/little-boy/little-boy8.png",
+    "/images/little-boy/little-boy9.png",
+  ];
+
+  const imgElse = "/images/avatar-de-licorne.png";
+
+  const getRandomImage = (images: string[]) =>
+    images[Math.floor(Math.random() * images.length)];
+
+  const imgSrc =
+    gender === "F"
+      ? getRandomImage(imgGirl)
+      : gender === "M"
+        ? getRandomImage(imgBoy)
+        : imgElse;
+
 
   const handleChange = (
     e: ChangeEvent<HTMLInputElement | HTMLSelectElement>,
