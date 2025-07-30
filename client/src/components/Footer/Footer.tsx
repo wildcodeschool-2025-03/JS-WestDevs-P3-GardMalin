f5ff8b8878ee2ec7480dff1af4d8a92dd34c8130
Romain-dev85 <r.auvinet@gmail.com>
2025-07-30 10:50:38 +0200

    footer modified

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -7,24 +7,24 @@ function Footer() {
       <ul className="link-footer">
         <li className="link-logo">
           <a href="https://fr-fr.facebook.com/" target="blank">
-            <img src="/images/facebook-logo.png" alt="logo Facebook" />
+            <img src="/images/facebook.png" alt="logo Facebook" />
           </a>
           <a
             href="https://github.com/wildcodeschool-2025-03/JS-WestDevs-P3-GardMalin"
             target="blank"
           >
-            <img src="/images/github-logo.png" alt="logo GitHub" />
+            <img src="/images/github.png" alt="logo GitHub" />
           </a>
           <a href="https://www.linkedin.com" target="blank">
-            <img src="/images/linkedin-logo.png" alt="logo Facebook" />
+            <img src="/images/linkedin.png" alt="logo linkedin" />
           </a>
         </li>
         <li className="link-pages">
           <p>
-            <Link to="">CGU</Link>
+            <Link to="/maintenance">CGU</Link>
           </p>
           <p>
-            <Link to="">Nous rejoindre</Link>
+            <Link to="/maintenance">Nous rejoindre</Link>
           </p>
           <p>
             <Link to="/about">A propos</Link>
